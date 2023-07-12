ee9127d627437a79e4d22844dcb4c972ea6a672e
SonMooSans <god63820869@gmail.com>
2023-07-12 16:04:57 +0800

    Integrate with Sanity Studio

---
+++ app/studio/[[...index]]/page.tsx
@@ -0,0 +1,8 @@
+import { Studio } from "./Studio";
+
+// Set the right `viewport`, `robots` and `referer` meta tags
+export { metadata } from "next-sanity/studio/metadata";
+
+export default function StudioPage() {
+    return <Studio />;
+}
--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_API_DATASET,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     apiVersion: "2022-03-25",
     useCdn: false,
 });
