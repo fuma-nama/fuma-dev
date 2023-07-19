01f3d73dc5cddfbdf0c4e6add96be6ac31e68209
SonMooSans <god63820869@gmail.com>
2023-07-19 23:14:44 +0800

    Hotifx: remove code highlighting

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,16 +1,5 @@
 import { PortableText } from "@portabletext/react";
 import { SpotifyEmbed } from "./spotify-embed";
-import { getHighlighter, setCDN } from "shiki";
-
-if (typeof window !== "undefined") {
-    // load from cdn if it's in client component
-    setCDN("https://unpkg.com/shiki");
-}
-
-const highlighter = getHighlighter({
-    langs: ["json", "sql", "javascript", "typescript"],
-    theme: "dracula",
-});
 
 export function PostBody({ value }: { value: any }) {
     return (
@@ -33,22 +22,5 @@ async function CodeBlock({
     language?: string;
     code: string;
 }) {
-    const tokens = (await highlighter).codeToThemedTokens(
-        code,
-        language === "mysql" ? "sql" : language
-    );
-
-    return (
-        <pre className="border">
-            {tokens.map((line, i) => (
-                <div key={i}>
-                    {line.map((token, key) => (
-                        <span key={key} style={{ color: token.color }}>
-                            {token.content}
-                        </span>
-                    ))}
-                </div>
-            ))}
-        </pre>
-    );
+    return <pre className="border">{code}</pre>;
 }
