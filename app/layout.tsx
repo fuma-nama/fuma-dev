6fd6fb8ec4ab83e6bc91a372927b3d137a3ebd4e
SonMooSans <god63820869@gmail.com>
2023-07-30 19:19:55 +0800

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Money Shark",
+    title: "Fuma Nama",
     description: "My Personal Website",
 };
 
